72605b09d04bbed4788467a2c611d1d673a76818
bag3318 <bglatman@outlook.com>
2017-03-24 08:05:39 -0700

    add comments

--- rps-mac.rb
+++ rps-mac.rb
@@ -1,61 +1,61 @@
 #!/usr/bin/env ruby
 
-NTRY_TO_SYM = { 'p'=>:paper, 'r'=>:rock, 's'=>:scissors }
-VALID_ENTRIES = NTRY_TO_SYM.keys
-COMPUTER_CHOICES = NTRY_TO_SYM.values
+NTRY_TO_SYM = { 'p'=>:paper, 'r'=>:rock, 's'=>:scissors } # define entries
+VALID_ENTRIES = NTRY_TO_SYM.keys # define valid entries
+COMPUTER_CHOICES = NTRY_TO_SYM.values # define computer choices
 # WINNERS and LOSERS from the player's perspective, the first value of each
 # pair being the player's choice, the second, the computer's choice.
-WINNERS = [[:scissors, :paper], [:paper, :rock], [:rock, :scissors]]
-LOSERS =  WINNERS.map { |i,j| [j,i] }
+WINNERS = [[:scissors, :paper], [:paper, :rock], [:rock, :scissors]] # define winners 
+LOSERS =  WINNERS.map { |i,j| [j,i] } # define loosers
 
 class RockPaperScissors # create class: Rock Paper Scissors
-  def initialize
-    @player_score = @computer_score = @ties = 0
+  def initialize # define all dynamic variables ...
+    @player_score = @computer_score = @ties = 0 # set them = to 0
   end
-  def play(winning_score)
-    while @player_score < winning_score && @computer_score < winning_score
+  def play(winning_score) # define play method; pass in winning_score
+    while @player_score < winning_score && @computer_score < winning_score # loop through the scores
       puts "Player score: #{@player_score}, " + 
-           "Computer score: #{@computer_score}, Ties: #{@ties}"
-      player = player_choice 
-      computer = COMPUTER_CHOICES.sample
-      puts "\nPlayer chooses #{player.to_s}"
-      puts "Computer chooses #{computer.to_s}"
+           "Computer score: #{@computer_score}, Ties: #{@ties}" # output scores
+      player = player_choice # define player variable; chooses user-defined input
+      computer = COMPUTER_CHOICES.sample # define copmuter variable; chooses input randomly
+      puts "\nPlayer chooses #{player.to_s}" # output what the player chooses
+      puts "Computer chooses #{computer.to_s}" # output computer choice
 
-      case player_outcome [player, computer]
-        when :WIN
+      case player_outcome [player, computer] # add a new case 
+        when :WIN # win condition
           puts "#{player.to_s} beats #{computer.to_s}, player wins the round"
-          @player_score += 1
-        when :LOSE
+          @player_score += 1 # add 1 point to the player
+        when :LOSE # lose condition
           puts "#{computer.to_s} beats #{player.to_s}, computer wins the round"
-          @computer_score += 1
-        else
+          @computer_score += 1 # add 1 point to the computer
+        else # else if it's a tie
           puts "Tie, choose again"
-          @ties += 1
+          @ties += 1 # add 1 point to the tie score
       end
     end
     puts "\nFinal score: player: #{@player_score}, " +
-         "computer: #{@computer_score} (ties: #{@ties})"
-    puts (@player_score == 2) ? "Player wins!" : "Yea! Computer wins!"
+         "computer: #{@computer_score} (ties: #{@ties})" # output final scores
+    puts (@player_score == 2) ? "Player wins!" : "Yea! Computer wins!" # output who wins
   end
-  private
+  private # make private
 
-  def player_choice
+  def player_choice # define the player's choice
     loop do
       print "Choose rock (r), paper (p) or scissors (s): "
-      choice = gets.chomp.downcase
-      return NTRY_TO_SYM[choice] if NTRY_TO_SYM.key?(choice)
-      puts "That entry is invalid. Please re-enter"
+      choice = gets.chomp.downcase # define choice variable
+      return NTRY_TO_SYM[choice] if NTRY_TO_SYM.key?(choice) # if not one of the valid entries
+      puts "That entry is invalid. Please re-enter" # output this
     end
   end
 
-  def player_outcome(plays)
-    return :WIN  if WINNERS.include?(plays)
-    return :LOSE if LOSERS.include?(plays)
-    :TIE
+  def player_outcome(plays) # define player outcome
+    return :WIN  if WINNERS.include?(plays) # return win condition
+    return :LOSE if LOSERS.include?(plays) # return lose condition
+    :TIE # tie
   end
 end
 
-RockPaperScissors.new.play(3)
+RockPaperScissors.new.play(3) # do rock paper scissor's play method 3 times
 
 =begin
 
