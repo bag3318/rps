1a9a65ad1d08b6eeca84e73ca70303fd0f5ca739
bag3318 <bglatman@outlook.com>
2017-03-25 08:49:30 -0700

    fix windows

--- rps.bat
+++ rps.bat
@@ -1,11 +1,8 @@
 @ECHO OFF
 
 :master
-	:echo_statement
-		ECHO Would you like to run the `rps-win.rb` file?
-		ECHO Note: you must have Ruby installed. If you don't, read the `README.md` file on Github for this repo for more info.
-	GOTO:EOF
-	CALL:echo_statement
+	ECHO Would you like to run the `rps-win.rb` file?
+	ECHO Note: you must have Ruby installed. If you don't, read the `README.md` file on Github for this repo for more info.
 	:choose
 		CHOICE /C YN
 		IF %ERRORLEVEL%==1 CALL:process
